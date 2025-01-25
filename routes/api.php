b4afe3600b205335df6823d9b8d7153c258a31cf
raphalcao <raphalcao@gmail.com>
2025-01-24 23:10:50 -0300

    feat/routes

--- routes/api.php
+++ routes/api.php
@@ -12,27 +12,7 @@ use App\Http\Controllers\AuthController;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/use App\Models\User; // Substitua pelo modelo da sua tabela
-
-Route::get('/users', function () {
-    // Consulta no banco de dados
-    $users = User::all();
-
-    // Retorna os resultados em formato JSON
-    return response()->json([
-        'status' => 'success',
-        'data' => $users,
-    ]);
-});
-
-Route::get('/teste', function () {
-    return response()->json([
-        'message' => 'Rota de teste funcionando!',
-        'status' => 'success',
-        'timestamp' => now(),
-    ]);
-});
-
+*/
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
